d4eff51b24b403838aa2433c433fe09e8cd44042
Masha Danilenko <samariya57@gmail.com>
2018-11-14 13:31:15 -0500

    Added schema

---
+++ src/db/schema.sql
@@ -0,0 +1,32 @@
+/* Clean Database */
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+/* Create a table for all possible combinations of zipcode and category */
+CREATE TABLE combinations (
+  CombinationID SERIAL,
+  Zipcode VARCHAR(15) NOT NULL,
+  Category VARCHAR(25) NOT NULL
+);
+
+/* Create a table for all restaurants */
+CREATE TABLE restaurants{
+  RestaurantID TEXT NOT NULL,
+  Address TEXT NOT NULL,
+  Name VARCHAR(255) NOT NULL,
+  Added TIMESTAMP NOT NULL
+}
+
+/* Create a table for queue or URLs*/
+CREATE TABLE queue{
+  Url TEXT NOT NULL,
+  Added TIMESTAMP NOT NULL,
+  Scrapped BOOLEAN NOT NULL
+}
+
+/* Create a table for IP tracker */
+CREATE TABLE ips{
+  IPID VARCHAR(25) NOT NULL,
+  Scrapped TIMESTAMP NOT NULL,
+  Count NUMBER
+}
--- src/scraper/yelp_reader.py
+++ src/scraper/yelp_reader.py
@@ -1,12 +1,14 @@
 # import libraries
-#import urllib2
-#from bs4 import BeautifulSoup
+import urllib2
+from bs4 import BeautifulSoup
 
-
+DEBUG=True
 
 def main():
     '''
-
+    Function for getting next url in a Queue, retrieve, parse, and store to the DB
+    return:
+    rtype:
     '''
     # get next combination (or subpage) from the DB
     current_url = "https://www.yelp.com/search?find_desc=thai&find_loc=New+York+10010&start=0&cflt=chinese"
@@ -16,6 +18,10 @@ def main():
     current_page = urllib2.urlopen(current_url)
     if DEBUG:
         print page.read()
+    parsed_page = BeautifulSoup(current_page, "html.parser")
+    if DEBUG:
+        print parsed_page
+
 
 if __name__ == '__main__':
    main()
