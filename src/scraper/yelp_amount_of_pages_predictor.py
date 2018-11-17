2977c9df69eb110b5cd1a6b348076e7ae9e14ac0
Masha Danilenko <samariya57@gmail.com>
2018-11-16 22:34:40 -0600

    Checked connection

--- src/scraper/yelp_amount_of_pages_predictor.py
+++ src/scraper/yelp_amount_of_pages_predictor.py
@@ -12,8 +12,9 @@ def get_connection():
     '''
     HOST=os.environ['HOST']
     PASSWORD=os.environ['PGPASSWORD']
+    configs = "host="+HOST+" port='5432' dbname=yelp user=airflow password="+PASSWORD
     try:
-        conn = psycopg2.connect("host="+HOST+" port='5432' dbname=yelp user=airflow password="+PASSWORD)
+        conn = psycopg2.connect(configs)
     except:
         print "Coudn't connect to the DB"
     return conn
@@ -26,7 +27,7 @@ def get_number_of_restaurants(category, zipcode):
     arguments: type of restaurant and Zipcode
     return: number restaurants per page, total number and date
     '''
-    current_url = "https://www.yelp.com/search?find_desc="+category+"&find_loc="+zipcode
+    current_url = "https://www.yelp.com/search?find_desc=food&find_loc=zipcode+"+zipcode+"&start=0&cflt="+category
     current_page = urllib2.urlopen(current_url)
     # Get number of restaurants for current criteria
     total_number = int(parsed_page.find('span', attrs={'class': 'pagination-results-window'}).text.split()[-1])
@@ -40,15 +41,7 @@ def main():
     return:
     rtype:
     '''
-    #conn = get_connection()
-    HOST=os.environ['HOST']
-    PASSWORD=os.environ['PGPASSWORD']
-    print HOST
-    print PASSWORD
-    configs = "host="+HOST+" port='5432' dbname=yelp user=airflow password="+PASSWORD
-    print configs
-    #conn = psycopg2.connect("host="+HOST+" port='5432' dbname=yelp user=airflow password="+PASSWORD)
-    conn = psycopg2.connect(configs)
+    conn = get_connection()
     cur = conn.cursor()
     sql_in_queue = "INSERT INTO queue (Zipcode, Category, NumberPerPage, TotalNumber, Added, Scrapped) VALUES (%s, %s, %s, %s, %s, %s);"
     # get lists of categories and zipcodes
@@ -59,7 +52,8 @@ def main():
     try:
         for category in categories:
             for zipcode in zipcodes:
-                current_numbers = get_number_of_restaurants(category, zipcode)
+                #current_numbers = get_number_of_restaurants(category, zipcode)
+                print current_numbers
                 cur.execute(sql_in_queue, (zipcode, category, current_numbers[0], current_numbers[1], today, False))
                 sleep(20)
     except:
